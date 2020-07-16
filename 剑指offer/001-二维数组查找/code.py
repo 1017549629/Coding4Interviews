ff91df8819482a5cf7cd82c5b3652f3f4d10ebbe
TianlongKong <65606373+TianlongKong@users.noreply.github.com>
2020-07-16 18:07:34 +0800

    Update code.py
    自己在leetcode上跑了一遍，添加的一些注意事项

--- 剑指offer/001-二维数组查找/code.py
+++ 剑指offer/001-二维数组查找/code.py
@@ -1,8 +1,8 @@
 
 def find(target, array):
     i = 0
-    j = len(array[0]) - 1
-    while i < len(array) and j >= 0:
+    j = len(array[0]) - 1 #这里len(array[0])只能使用array[0],因为这个二维数组的维度没有定义，也可以是假二维真一维np.array([[1], [4]])
+    while i < len(array) and j >= 0: #也可以是i <= len(array)-1 and j >= 0
         base = array[i][j]
         if target == base:
             return True
