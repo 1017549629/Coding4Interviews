19e94575ed913eec0e8796b11f568a9b91fc72d9
TianlongKong <65606373+TianlongKong@users.noreply.github.com>
2020-07-16 19:34:46 +0800

    Update code.py
    更新了原作者不能使用的脚本

--- 剑指offer/003-从尾到头打印链表/code.py
+++ 剑指offer/003-从尾到头打印链表/code.py
@@ -1,11 +1,30 @@
 
+
+#可以成功的脚本
+# 运行时间：24ms
+# 占用内存：5752k
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        if listNode == None:
+            return []
+        result = []
+        while listNode != None:
+            result.append(listNode.val)
+            listNode = listNode.next
+        return result[::-1]
+
+
+
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
-
-# 解法1：用辅助栈存储
+#我尝试运行了有错误，下面的别人的脚本可以运行，没有使用init
+# 解法1：用辅助栈存储 
 def printListFromTailToHead(listNode):
     # write code here
     stack = []
@@ -17,8 +36,6 @@ def printListFromTailToHead(listNode):
     while stack:
         result_array.append(stack.pop(-1))
     return result_array
-
-
 # 解法2：本身栈调用
 result_array = []
 def printListFromTailToHead(listNode):
@@ -27,3 +44,7 @@ def printListFromTailToHead(listNode):
         printListFromTailToHead(listNode.next)
         result_array.append(listNode.val)
     return result_array
+
+
+
+
